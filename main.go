965159bbcc06fd79f22b886c47d3e39257a3d002
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-11-10 14:07:37 +0100

    PVs: serialize objects as yml and store them in the file

--- main.go
+++ main.go
@@ -1,32 +1,91 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"fmt"
+	"os"
+	"path/filepath"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/serializer/json"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
 	corev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	"k8s.io/client-go/rest"
 )
 
+var serializer = json.NewYAMLSerializer(json.DefaultMetaFactory, scheme.Scheme,
+	scheme.Scheme)
+
 // Clone the repo and return the path
 func cloneRepo() string {
-	return "/foo"
+	return "/repo"
 }
 
-func storePersistentVolumes(client corev1.CoreV1Interface, path string) {
+func storeObject(buffer *bytes.Buffer, name string, pathPrefix string) error {
+	filePath, err := filepath.Abs(filepath.Join(pathPrefix, fmt.Sprintf("%s.yml", name)))
+	if err != nil {
+		return err
+	}
+
+	// Create file
+	f, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	w := bufio.NewWriter(f)
+	_, err = buffer.WriteTo(w)
+	if err != nil {
+		return err
+	}
+	w.Flush()
+
+	fmt.Println("\tStored ", name, " in ", filePath)
+	return nil
+}
+
+func storePersistentVolumes(client corev1.CoreV1Interface, path string, serializer *json.Serializer) {
+	fmt.Println("Storing PersistentVolumes")
+
+	// Make directory for persistent volumes
+	pathPrefix := filepath.Join(path, "persistentvolumes")
+	os.Mkdir(pathPrefix, os.ModePerm)
+	fmt.Println("\tCreated ", pathPrefix)
+
+	// List volumes
 	pvList, err := client.PersistentVolumes().List(metav1.ListOptions{})
 	if err != nil {
-		panic(err.Error())
+		panic(err)
 	}
+
+	// Save each volume
 	for _, pv := range pvList.Items {
-		fmt.Println("Found ", pv.Name)
+		name := pv.Name
+		var buf bytes.Buffer
+		err = serializer.Encode(&pv, &buf)
+		if err != nil {
+			// TODO collect errs here instead of panicking
+			panic(err)
+		}
+		err = storeObject(&buf, name, pathPrefix)
+		if err != nil {
+			// TODO collect errs here instead of panicking
+			panic(err)
+		}
 	}
 }
 
 // Store global k8s objects
-func storeGlobals(client corev1.CoreV1Interface, path string) {
-	storePersistentVolumes(client, path)
+func storeGlobals(client corev1.CoreV1Interface, path string, serializer *json.Serializer) {
+	// Make directory for globals
+	globalsPath := filepath.Join(path, "globals")
+	os.Mkdir(globalsPath, os.ModePerm)
+	fmt.Println("Created ", globalsPath)
+
+	storePersistentVolumes(client, globalsPath, serializer)
 
 }
 
@@ -50,10 +109,10 @@ func gitPush(path string) {
 
 }
 
-func saveClusterState(clientset *kubernetes.Clientset) {
+func saveClusterState(clientset *kubernetes.Clientset, serializer *json.Serializer) {
 	var client = clientset.CoreV1()
 	path := cloneRepo()
-	storeGlobals(client, path)
+	storeGlobals(client, path, serializer)
 	namespaces := getNamespaces(client)
 	for _, namespace := range namespaces {
 		storeNamespaces(namespace, path)
@@ -75,6 +134,6 @@ func main() {
 	}
 	// Read k8k8state2git config here
 
-	saveClusterState(clientset)
+	saveClusterState(clientset, serializer)
 
 }
